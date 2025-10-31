ec8d14c96c4ad478e115704d8715127e7b3a19b3
Martinf88 <143597591+Martinf88@users.noreply.github.com>
2025-10-31 10:26:22 +0100

    completes exercises 1 - 10 exept 8

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
+/*TODO: Repeat this exercise several times */
+
+const removeFromArray = function(array, ...args) {
+
+    
+    
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function(val1, val2) {
+    if (!Number.isInteger(val1) || !Number.isInteger(val2)) return "ERROR"
+    if(val1 < 0 || val2 < 0) return "ERROR"
 
+    const numbersArray = []
+    numbersArray.push(val1, val2)
+    numbersArray.sort()
+    
+    let sum = 0;
+    for (let i = numbersArray[0]; i <= numbersArray[1]; i++) {
+        sum += i
+    }
+
+    return sum
 };
 
 // Do not edit below this line
