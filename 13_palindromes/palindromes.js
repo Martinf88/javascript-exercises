6a8edbe99f1f4ad573e62a0ff11f4da4d09b91b8
Martinf88 <143597591+Martinf88@users.noreply.github.com>
2026-02-01 10:40:50 +0100

    completes 13_palindromes

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  const alphanumerical = "abcdefghijklmnopqrstuvwxyz0123456789";
 
+  const cleanedString = str
+    .toLowerCase()
+    .split("")
+    .filter((char) => alphanumerical.includes(char))
+    .join("");
+
+  const reversedString = cleanedString.split("").reverse().join("");
+
+  return reversedString === cleanedString;
 };
 
 // Do not edit below this line
